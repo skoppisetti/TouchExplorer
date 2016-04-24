de5c18b17604202ed7eacb8f1201595f26f2e60f
Suresh Koppisetti <suresh.koppisetti@gmail.com>
2016-04-23 21:00:12 -0400

    Simple app to demonstrate use of tap and touch gestures.

--- TouchExplorer/ViewController.swift
+++ TouchExplorer/ViewController.swift
@@ -10,16 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messagesLabel: UILabel!
+    @IBOutlet weak var tapsLabel: UILabel!
+    @IBOutlet weak var touchesLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    /*
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    */
+    
+    func updateLablesFromTouches(touches: NSSet!){
+        let touch = touches.anyObject() as! UITouch
+        let numTaps = touch.tapCount
+        let tapsMessage = "\(numTaps) taps detected"
+        tapsLabel.text = tapsMessage
+        
+        let numTouches = touches.count
+        let touchMsg = "\(numTouches) touches detected"
+        touchesLabel.text = touchMsg
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        messagesLabel.text = "Touches Began"
+        updateLablesFromTouches(event?.allTouches())
+    }
+    
+    override func touchesCancelled(touches: Set<UITouch>?, withEvent event: UIEvent?) {
+        messagesLabel.text = "Touches Cancelled"
+        updateLablesFromTouches(event?.allTouches())
+    }
+    
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        messagesLabel.text = "Touches Ended"
+        updateLablesFromTouches(event?.allTouches())
+    }
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        messagesLabel.text = "Drag Detected"
+        updateLablesFromTouches(event?.allTouches())
+    }
 
 }
 
